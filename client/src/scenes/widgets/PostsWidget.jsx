ad8f0a69c523a5e1b11c5b4aae8538019214432d
IsmailGN <84373556+IsmailGN@users.noreply.github.com>
2023-03-29 01:30:43 +0400

    second

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 import { setPosts } from "state";
 import PostWidget from "./PostWidget";
 
